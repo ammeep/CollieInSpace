4b3d6834dd214d1177706302cc373ff9aadd05b9
Amy Palamountain <amy@palamounta.in>
2013-02-18 21:11:10 +1300

    Moved the game out of the global scope

--- js/main.js
+++ js/main.js
@@ -1,44 +1,48 @@
-
-var collieTheExplorer = (function ( $, vent ) {
+(function(global, $, vent){
+	
+	global.game = {};
+	
+	var collieTheExplorer = (function () {
   
-  	var gameStage;
-	var gameContainer;
-	  
-    function initialiseCollieImageManager(){
-        collie.ImageManager.add({
-			rabbit:   "images/rabbit_walk.png",
-			ground:   "images/ground.png",
-			sky:	  "images/sky.png",
-			cloud2:	  "images/cloud2.png",
-			mountain: "images/mountain.png",
-			mushroom: "images/mushroom.png"
-		});
-    }
-
-    function initialiseGameCanvas(){
-       gameContainer = $('#container');
-       gameContainer.css('width', window.innerWidth + 'px');      
-       gameContainer.css('height', window.innerHeight + 'px');
-    }
-    
-    function initialiseGameObjects(){
-    	var layers;
-       layers = [background.layer(),ground.layer(),enemies.layer(),player.layer()];
-       gameStage = new stage(gameContainer,layers,enemies, player);
-    }
-    
-    return{
-        startGame: function(){
-            initialiseCollieImageManager();   
-            initialiseGameCanvas();    
-            initialiseGameObjects();
-            gameStage.start();         
-        }            
-    };
+	  	var gameStage;
+		var gameContainer;
+		  
+	    function initialiseCollieImageManager(){
+	        collie.ImageManager.add({
+				rabbit:   "images/rabbit_walk.png",
+				ground:   "images/ground.png",
+				sky:	  "images/sky.png",
+				cloud2:	  "images/cloud2.png",
+				mountain: "images/mountain.png",
+				mushroom: "images/mushroom.png"
+			});
+	    }
+	
+	    function initialiseGameCanvas(){
+	       gameContainer = $('#container');
+	       gameContainer.css('width', window.innerWidth + 'px');      
+	       gameContainer.css('height', window.innerHeight + 'px');
+	    }
+	    
+	    function initialiseGameObjects(){
+	    	var layers;
+	       layers = [background.layer(),ground.layer(),enemies.layer(),player.layer()];
+	       gameStage = new stage(gameContainer,layers,enemies, player);
+	    }
+	    
+	    return{
+	        startGame: function(){
+	            initialiseCollieImageManager();   
+	            initialiseGameCanvas();    
+	            initialiseGameObjects();
+	            gameStage.start();         
+	        }            
+	    };
    
-}( jQuery, PubSub ));
-
-
-$(document).ready(function(){	
-	collieTheExplorer.startGame();  
-});
+    })();
+	
+	$(document).ready(function () {
+		collieTheExplorer.startGame();  
+	});
+	
+})(window, jQuery, PubSub);
