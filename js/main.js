34b027840c54c918ded7edbd645daf9c03935e5b
Amy Palamountain <amy@palamounta.in>
2013-02-19 17:04:24 +1300

    Enemies into own module
    refactored enemies off the global scope.

--- js/main.js
+++ js/main.js
@@ -37,14 +37,14 @@
 	    
 	    function initialiseGameObjects(){
 	    
-	       var layers = [background.layer(),ground.layer(),enemies.layer(),player.layer()];
+	       var layers = [background.layer(),ground.layer(),game.enemies.layer(),player.layer()];
 	       
 	       $.each(layers,function(index,value){
 				collie.Renderer.addLayer(value);
 		   });				
 		   
 		   collie.Renderer.load(gameContainer[0]);
-	       gameEngine = new game.engine(gameContainer,enemies, player);
+	       gameEngine = new game.engine(gameContainer,game.enemies, player);
 	    }
 	    
 	    return{
